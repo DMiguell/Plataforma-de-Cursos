961103770faca92d15e53c8fe4aac098012185a3
DMiguell <86971868+DMiguell@users.noreply.github.com>
2022-03-02 11:20:23 -0500

    first commit

---
+++ database/seeders/PlatformSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Platform;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PlatformSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        Platform::create([
+            'name' => 'Youtube'
+        ]);
+
+        Platform::create([
+            'name' => 'Vimeo'
+        ]);
+    }
+}
