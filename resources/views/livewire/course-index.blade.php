10a7e6c58fa4a3ff23f97789b3ee5ca315a1537e
DMiguell <86971868+DMiguell@users.noreply.github.com>
2022-03-03 18:51:48 -0500

    Habilitar filtrado de cursos y paginacion

--- resources/views/livewire/course-index.blade.php
+++ resources/views/livewire/course-index.blade.php
@@ -2,7 +2,7 @@
 
     <div class="bg-gray-200 py-4 mb-16">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex">
-            <button class="bg-white shadow h-12 px-4 rounded-lg text-gray-700 mr-4">
+            <button class="bg-white shadow h-12 px-4 rounded-lg text-gray-700 mr-4 focus:outline-none" wire:click="resetFilters">
                 <i class="fas fa-archway text-xs mr-2"></i>
                 Todos los Cursos
             </button>
@@ -16,13 +16,9 @@
                 </button>
                 {{-- Dropdown-Body --}}
                 <div x-show="open" x-on:click.away="open = false" class="absolute right-0 w-40 mt-2 py-2 bg-white border rounded shadow-x1 ">
-                    <a href="" class="transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white">Settings</a>
-                    <div class="py-2">
-                        <hr>
-                    </div>
-                    <a href="" class="transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white">
-                        Logout
-                    </a>
+                    @foreach ($categories as $category)
+                    <a class="cursor-pointer transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white" wire:click="$set('category_id', {{$category->id}})" x-on:click="open = false">{{$category->name}}</a>
+                    @endforeach                    
                 </div>
             </div>
 
@@ -35,13 +31,11 @@
                 </button>
                 {{-- Dropdown-Body --}}
                 <div x-show="open" x-on:click.away="open = false" class="absolute right-0 w-40 mt-2 py-2 bg-white border rounded shadow-x1 ">
-                    <a href="" class="transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white">Settings</a>
-                    <div class="py-2">
-                        <hr>
-                    </div>
-                    <a href="" class="transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white">
-                        Logout
-                    </a>
+                    @foreach ($levels as $level)
+                    <a class="cursor-pointer transition-colors duration-200 block px-4 py-2 text-normal text-gray-900 rounded hover:bg-purple-500 hover:text-white" wire:click="$set('level_id', {{$level->id}})" x-on:click="open = false" >{{$level->name}}</a>
+                    @endforeach
+                    
+                    
                 </div>
             </div>
 
@@ -50,28 +44,7 @@
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
         @foreach ($courses as $course)
-            <article class="bg-white shadow-lg rounded overflow-hidden">
-                <img class="h-36 w-full object-cover" src="{{Storage::url($course->image->url)}}" alt="">
-                <div class="px-6 py-4">
-                    <h1 class="text-xl text-gray-700 mb-2 leading-6">{{Str::limit($course->title, 40)}}</h1>
-                    <p class="text-gray-500 text-sm mb-2">Prof: {{$course->teacher->name}}</p>
-                    <div class="flex">
-                        <ul class="flex text-sm">
-                            <li class="mr-1" ><i class="fas fa-star text-{{$course->rating >= 1 ? 'yellow' : 'gray' }}-400"></i></li>
-                            <li class="mr-1" ><i class="fas fa-star text-{{$course->rating >= 2 ? 'yellow' : 'gray' }}-400"></i></li>
-                            <li class="mr-1" ><i class="fas fa-star text-{{$course->rating >= 3 ? 'yellow' : 'gray' }}-400"></i></li>
-                            <li class="mr-1" ><i class="fas fa-star text-{{$course->rating >= 4 ? 'yellow' : 'gray' }}-400"></i></li>
-                            <li class="mr-1" ><i class="fas fa-star text-{{$course->rating == 5 ? 'yellow' : 'gray' }}-400"></i></li>
-                        </ul> 
-
-                        <p class="text-sm text-gray-500 ml-auto">
-                            <i class="fas fa-users"></i>
-                            {{$course->students_count}}
-                        </p>
-                    </div>
-                    <a href="{{route('courses.show', $course)}}" class=" w-full bg-blue-500 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block text-center">Mas informacion</a>
-                </div>
-            </article>
+            <x-course-card :course="$course" />
         @endforeach
     </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <x-app-layout>
+
     {{-- PORTADA --}}
+
     <section class="bg-cover" style="background-image: url({{asset('img/home/people-ga847f2c7d_1920.jpg')}})">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
@@ -20,6 +22,7 @@
             </div>     
         </div>
     </section>{{-- PORTADA --}}
+
 
     <section class="mt-24">
         <h1 class="text-gray-600 text-center text-3xl uppercase">Contenido</h1>
@@ -80,28 +83,7 @@
         
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
             @foreach ($courses as $course)
-                <article class="bg-white shadow-lg rounded overflow-hidden">
-                    <img class="h-36 w-full object-cover" src="{{Storage::url($course->image->url)}}" alt="">
-                    <div class="px-6 py-4">
-                        <h1 class="text-xl text-gray-700 mb-2 leading-6">{{Str::limit($course->title, 40)}}</h1>
-                        <p class="text-gray-500 text-sm mb-2">Prof: {{$course->teacher->name}}</p>
-                        <div class="flex">
-                            <ul class="flex text-sm">
-                                <li class="mr-1" ><i class="fas fa-star text-{{$course->rating >= 1 ? 'yellow' : 'gray' }}-400"></i></li>
-                                <li class="mr-1" ><i class="fas fa-star text-{{$course->rating >= 2 ? 'yellow' : 'gray' }}-400"></i></li>
-                                <li class="mr-1" ><i class="fas fa-star text-{{$course->rating >= 3 ? 'yellow' : 'gray' }}-400"></i></li>
-                                <li class="mr-1" ><i class="fas fa-star text-{{$course->rating >= 4 ? 'yellow' : 'gray' }}-400"></i></li>
-                                <li class="mr-1" ><i class="fas fa-star text-{{$course->rating == 5 ? 'yellow' : 'gray' }}-400"></i></li>
-                            </ul> 
-
-                            <p class="text-sm text-gray-500 ml-auto">
-                                <i class="fas fa-users"></i>
-                                {{$course->students_count}}
-                            </p>
-                        </div>
-                        <a href="{{route('courses.show', $course)}}" class=" w-full bg-blue-500 mt-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block text-center">Mas informacion</a>
-                    </div>
-                </article>
+                <x-course-card :course="$course" />
             @endforeach
         </div>
     
