681e99c13bc3c080f28513c64a5642019cdb9fe9
DMiguell <86971868+DMiguell@users.noreply.github.com>
2022-03-04 16:29:09 -0500

    Recuperando informacion que se va a mostrar

--- app/Http/Livewire/CourseStatus.php
+++ app/Http/Livewire/CourseStatus.php
@@ -8,10 +8,19 @@ use App\Models\Course;
 class CourseStatus extends Component
 {
     public $course;
+    public $current;
 
     public function mount(Course $course)
     {
         $this->course = $course;
+        foreach ($course->lessons as $lesson)
+        {
+            if(!$lesson->completed)
+            {
+                $this->current = $lesson;
+                break;
+            }
+        }
     }
 
     public function render()
--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -1,3 +1,41 @@
-<div>
-    <h1>{{$course->title}}</h1>
+<div class="mt-8">
+    <div class="container grid grid-cols-3 gap-8">
+        <div class="col-span-2">
+            {!!$current->iframe!!}
+            {{$current->name}}
+        </div>
+
+        <div class="col-span-1 card">   
+            <div class="card-body">
+                <h1>{{$course->title}}</h1>
+                <div class="flex items-center">
+                    <figure>
+                        <img src="{{$course->teacher->profile_photo_url}}" alt="">
+                    </figure>
+                    <div class="">
+                        <p>{{$course->teacher->name}}</p>
+                        <a href="" class="text-blue-500">{{'@' . Str::slug($course->teacher->name,'')}}</a>
+                    </div>
+
+                </div>
+
+                <ul>
+                    @foreach ($course->sections as $section)
+                        <li>
+                            <a class="font-bold" href="">{{$section->name}}</a>
+                            <ul>
+                                @foreach ($section->lessons as $lesson)
+                                    <li>
+                                        <a href="">{{$lesson->id}} @if ($lesson->completed)
+                                            (Completado)
+                                        @endif</a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
