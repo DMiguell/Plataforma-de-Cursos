6f2f38547dc403367689da26e09ed88a75aa4f18
DMiguell <86971868+DMiguell@users.noreply.github.com>
2022-03-04 12:54:33 -0500

    Habilitar el buscador de cursos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,7 @@
                 <p class="text-white text-lg mt-2 mb-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis impedit neque fuga repellat dolorum! Et, autem corporis provident ipsa quos corrupti voluptatibus eligendi veritatis, explicabo impedit iste aut quibusdam. Dolor.</p>
                 
                 
-                <div class="pt-2 relative mx-auto text-gray-600">
-                    <input class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                    type="search" name="search" placeholder="Search">
-                    
-                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute right-0 top-0 mt-2">Buscar</button>
-                    
-                </div>
+                @livewire('search')
 
 
 
