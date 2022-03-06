5e5bee7cafc9651f0aa4de94cf8d4169c51c5cce
DMiguell <86971868+DMiguell@users.noreply.github.com>
2022-03-05 21:39:21 -0500

    Habilitar Botones next y previous

--- app/Http/Livewire/CourseStatus.php
+++ app/Http/Livewire/CourseStatus.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Livewire;
 
-use Livewire\Component;
 use App\Models\Course;
+use App\Models\Lesson;
+use Livewire\Component;
 
 class CourseStatus extends Component
 {
-    public $course;
-    public $current;
+    public $course, $current;
+    
+    
 
     public function mount(Course $course)
     {
         $this->course = $course;
+        
         foreach ($course->lessons as $lesson)
         {
             if(!$lesson->completed)
@@ -26,5 +29,39 @@ class CourseStatus extends Component
     public function render()
     {
         return view('livewire.course-status');
+    }
+
+    public function changeLesson(Lesson $lesson)
+    {
+        $this->current = $lesson;
+        
+
+    }
+
+    public function getIndexProperty()
+    {
+        return $this->course->lessons->pluck('id')->search($this->current->id);
+    }
+    public function getPreviousProperty()
+    {
+        if($this->index == 0)
+        {   
+            return null;
+        }
+        else
+        {
+            return $this->course->lessons[$this->index - 1];
+        }
+    }
+    public function getNExtProperty()
+    {
+        if($this->index == $this->course->lessons->count() - 1)
+        {   
+            return null;
+        }
+        else
+        {
+            return $this->course->lessons[$this->index + 1];
+        }
     }
 }
