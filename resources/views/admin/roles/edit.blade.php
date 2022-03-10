0c22b8391c74c5eaf0961e6fcf6af2aa89755598
Miguel Lopez Ticona <86971868+DMiguell@users.noreply.github.com>
2022-03-10 15:38:45 -0500

    Crear crud para roles

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -3,11 +3,22 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Editar Rol</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="card-body">
+        {!! Form::model($role, ['route' => ['admin.roles.update', $role], 'method' => 'put']) !!}
+
+            @include('admin.roles.partials.form')
+
+            
+
+            {!! Form::submit('Actualizar Role', ['class' => 'btn btn-primary mt-2']) !!}
+
+        {!! Form::close() !!}
+    </div>
+    </div>
 @stop
 
 @section('css')
