e5f390c7db8bd5248515d2fbb23c79cd297e5e6a
Miguel Lopez Ticona <86971868+DMiguell@users.noreply.github.com>
2022-03-22 16:18:59 -0500

    Reorganizar rutas

--- routes/instructor.php
+++ routes/instructor.php
@@ -1,9 +1,10 @@
 <?php
 
-
+use App\Http\Controllers\Instructor\CourseController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Livewire\InstructorCourses;
+
 
 Route::redirect('', 'instructor/courses');
 
-Route::get('courses', InstructorCourses::class)->middleware('can:Leer cursos')->name('instructor.courses.index');
+
+Route::resource('courses', CourseController::class)->names('instructor.courses');
